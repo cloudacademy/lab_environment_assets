6c5f8c7bbb96374d51ac5d00bfc65cb08746f939
whelmed <ben@bignoyeslab.com>
2024-12-16 13:12:16 -0600

    app mods

--- docker-skill-assessment/challenge_2/app.py
+++ docker-skill-assessment/challenge_2/app.py
@@ -10,68 +10,64 @@ DB_INIT = os.environ.get('DB_INIT', 0)
 CONNECTION_STRING = f"host={DB_HOST} user={DB_USER} dbname={DB_NAME} password={DB_PASS}"
 
 
-def table_exists() -> bool:
-    with psycopg.connect(CONNECTION_STRING) as conn:
-        with conn.cursor() as cur:
-            # Ensure the rainbow table exists.
-            cur.execute("""SELECT EXISTS (
-                SELECT FROM information_schema.tables
-                WHERE table_name = 'rainbow'
-            )""")
-            return cur.fetchone()[0]
-        
+# Helper functions
+
+# context manager to handle database connections
+def conn_cursor():
+    try:
+        with psycopg.connect(CONNECTION_STRING) as conn:
+            with conn.cursor() as cur:
+                yield conn, cur 
+    finally:
+        pass
+
     
 def table_filled() -> bool:
-    with psycopg.connect(CONNECTION_STRING) as conn:
-        with conn.cursor() as cur:
-            # Ensure the rainbow table is filled.
-            cur.execute("""SELECT COUNT(*) FROM rainbow WHERE num BETWEEN 1 AND 39""")
-            return cur.fetchone()[0] == 20
+    with conn_cursor() as (_, cur):
+        # Ensure the rainbow table is filled.
+        cur.execute("""SELECT COUNT(*) FROM rainbow WHERE num BETWEEN 1 AND 39""")
+        return cur.fetchone()[0] == 20
 
 
 def ensure_init() -> None:
     if not bool(DB_INIT):
         return
     
-    # Create the database, if it doesn't exist
-    with psycopg.connect(CONNECTION_STRING) as conn:
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
-            if not table_exists():
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS rainbow (
-                        rid SERIAL PRIMARY KEY,
-                        num INTEGER
-                    )
-                    """
-                )
+    with conn_cursor() as (conn, cur):
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS rainbow (
+                rid SERIAL PRIMARY KEY,
+                num INTEGER
+            )
+            """
+        )
 
-            if not table_filled():
-                cur.executemany("""INSERT INTO rainbow (num) VALUES (%s)""", [
-                    [1],
-                    [3],
-                    [5],
-                    [7],
-                    [9],
-                    [11],
-                    [13],
-                    [15],
-                    [17],
-                    [19],
-                    [21],
-                    [23],
-                    [25],
-                    [27],
-                    [29],
-                    [31],
-                    [33],
-                    [35],
-                    [37],
-                    [39],
-                ])
+        if not table_filled():
+            cur.executemany("""INSERT INTO rainbow (num) VALUES (%s)""", [
+                [1],
+                [3],
+                [5],
+                [7],
+                [9],
+                [11],
+                [13],
+                [15],
+                [17],
+                [19],
+                [21],
+                [23],
+                [25],
+                [27],
+                [29],
+                [31],
+                [33],
+                [35],
+                [37],
+                [39],
+            ])
 
-            # Make the changes to the database persistent
-            conn.commit()
+        # Make the changes to the database persistent
+        conn.commit()
 
 ###############################################################################
 from flask import Flask
