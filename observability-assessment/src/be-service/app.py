a3af199071eb67be1ef32e25949a2a6e1ff5fd30
@sowhelmed <ben@bignoyeslab.com>
2025-03-03 15:52:50 -0500

    bug fixes

--- observability-assessment/src/be-service/app.py
+++ observability-assessment/src/be-service/app.py
@@ -1,5 +1,6 @@
 import os
 from collections import namedtuple
+from unittest.mock import MagicMock
 
 from flask import (
     Flask, 
@@ -16,10 +17,33 @@ db_service = namedtuple('db_service', 'host port'.split())
 db_service.host = os.getenv('DB_SERVICE_HOST', 'db_service')
 db_service.port = os.getenv('DB_SERVICE_PORT', 9000)
 
+###############################################################################
+####### Task: 
+#######
+####### Create a counter that is incremented for each call of the make_url function.
+####### The urls_made variable is currently bound to a mock object that enables 
+####### calls to made to disparate methods without raising an exception.
+#######
+####### REQUIREMENTS 
+####### 
+####### 1.) Set the urls_made binding below to a otel Counter with the name: urls.made
+#######     The description can be set to an empty string. 
+#######
+####### Documentation is available at: 
+####### https://opentelemetry.io/docs/languages/python/getting-started/#metrics
+#######
+#####################
+from opentelemetry import metrics
+
+urls_made = MagicMock()
+
+
 def make_url(path: str=None):
     path = (path or '').lstrip('/') # Ensure any preceeding slash is removed.
-    
-    return f'http://{db_service.host}:{db_service.port}/{path}'
+    path = f'http://{db_service.host}:{db_service.port}/{path}'
+    # Increment the urls.made counter and include an attribute named url set to the value of the path binding.
+    urls_made.add(1, {'url': path})
+    return path
 
 @app.route('/')
 def index():
